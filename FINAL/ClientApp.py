c4340f26848cf73f2791d38160893a5174d1a201
JIMOHMA <mail4ayodele@gmail.com>
2023-04-14 17:11:37 -0400

    Implementing classes for the AppManager class and KeyInterrupt is working fine

--- FINAL/ClientApp.py
+++ FINAL/ClientApp.py
@@ -2,59 +2,6 @@ import zmq
 import sys
 import threading
 
-
-# ?????????????????????????????????????????????????????????????????????????????
-# ?????????????????????????????????????????????????????????????????????????????
-
-# def tprint(msg):
-#     """like print, but won't get newlines confused with multiple threads"""
-#     sys.stdout.write(msg + '\n')
-#     sys.stdout.flush()
-
-# class ClientTask(threading.Thread):
-#     """ClientTask"""
-#     def __init__(self, clientName):
-#         self.clientName = clientName
-#         threading.Thread.__init__ (self)
-
-#     def run(self):
-#         context = zmq.Context()
-#         socket = context.socket(zmq.DEALER)
-#         identity = u'%s' % self.clientName
-#         socket.identity = identity.encode('ascii')
-#         socket.connect('tcp://localhost:6670')
-#         print('Client-%s started' % (identity))
-#         poll = zmq.Poller()
-#         poll.register(socket, zmq.POLLIN)
-#         # reqs = 0  ## this will be an input from the user
-#         while True:
-#             # reqs = reqs + 1
-#             clientMsg = input("Enter your message here: ")
-#             print('{ %s } has been sent..' % (clientMsg))
-#             socket.send_string(u'%s' % (clientMsg))
-#             for i in range(5):
-#                 sockets = dict(poll.poll(1000))
-#                 if socket in sockets:
-#                     msg = socket.recv()
-#                     tprint('%s: %s' % ("AppManager:", msg))
-
-#         socket.close()
-#         context.term()
-
-# def main():
-
-#     clientName = input("Please enter the client's name or ID: ")
-#     client = ClientTask(clientName)
-#     client.start()
-    
-# if __name__ == "__main__":
-#     main()
-
-
-# ?????????????????????????????????????????????????????????????????????????????
-# ?????????????????????????????????????????????????????????????????????????????
-
-
 # Simple Dealer Socket
 context = zmq.Context()
 client = context.socket(zmq.DEALER)
@@ -65,7 +12,6 @@ client.connect('tcp://localhost:9999')
 # client sent an information
 clientID = input("Enter the your ID or userName: ")
 identity = u'%s' % clientID
-print("identity is: ", type(identity), identity)
 client.identity = identity.encode('ascii')
 
 client_sub_Socket = context.socket(zmq.SUB)
@@ -75,35 +21,44 @@ client_sub_Socket.setsockopt_string(zmq.SUBSCRIBE, '')
 
 # sending message function
 def sending_messages(client, clientID):
-  while True:
-    message = input()
+  try:
+    while True:
+      message = input()
 
-    list_Msg = [bytes(clientID, 'utf-8'), bytes(message, 'utf-8')]
-    client.send_multipart(list_Msg)
-    # client.send_string(u'%s' % (message)) # Implementation for the DEALER on the server side
-    # client.send_multipart([u'%s' % (message)]) # Implementation for a ROUTER on the server side
-    print("You: " + message)
+      list_Msg = [bytes(clientID, 'utf-8'), bytes(message, 'utf-8')]
+      client.send_multipart(list_Msg)
+      # client.send_string(u'%s' % (message)) # Implementation for the DEALER on the server side
+      # client.send_multipart([u'%s' % (message)]) # Implementation for a ROUTER on the server side
+      print("You: " + message)
+  except:
+    print("W: interrupt received, stopping...")
+  return
+
 
 # receiving message function
 def receiving_messages(client, client_sub_Socket):
   while True:
-    string_Msg = [msg.decode('utf-8') for msg in client_sub_Socket.recv_multipart()]
-    
-    # I
-    if len(string_Msg) == 1:
-      print(f'AppManager: {string_Msg[0]}')
-    elif len(string_Msg) == 0:
-      pass
-    else:
-      print(x for x in string_Msg)
-    # print("%s: %s" % ("AppManager", client_sub_Socket.recv_multipart()))
+    try:
+      string_Msg = [msg.decode('utf-8') for msg in client_sub_Socket.recv_multipart()]
+      # I
+      if len(string_Msg) == 1:
+        print(f'AppManager: {string_Msg[0]}')
+      elif len(string_Msg) == 0:
+        pass
+      else:
+        print(x for x in string_Msg)
+      # print("%s: %s" % ("AppManager", client_sub_Socket.recv_multipart()))
+    except:
+      print("W: interrupt received, STOPPING...")
+  return
 
-
-# threads to start both receving and sending functions
-threading.Thread(target=receiving_messages, args=(client, client_sub_Socket,)).start()
-threading.Thread(target=sending_messages, args=(client, clientID,)).start()
-
-
-# TODO: Handle ctrl-C 
+# threads to start both receving and sending messages
+# since we're using the command line for input from the user, using two threads
+# to simulate this interaction between the AppManager and Client {N} was required.
+# A sending action is not prevented by a receiving action
+t1 = threading.Thread(target=receiving_messages, args=(client, client_sub_Socket,))
+t1.start()
+t2 = threading.Thread(target=sending_messages, args=(client, clientID,))
+t2.start()
 
 # TODO: Create a pretty-print function for handling bytes and strings dynamically
--- FINAL/Manager.py
+++ FINAL/Manager.py
@@ -5,141 +5,87 @@ import threading
 import time
 from random import randint, random
 
-# ?????????????????????????????????????????????????????????????????????????????
-# ?????????????????????????????????????????????????????????????????????????????
+# Inheriting from the thread class 
+# Multithreading to be used for receiving and sending of messages
+class AppManager(threading.Thread):
 
-# __author__ = "Felipe Cruz <felipecruz@loogica.net>"
-# __license__ = "MIT/X11"
+  def __init__(self):
+    threading.Thread.__init__ (self)
+    
+    # Simple DEALER Socket for receiving asynchronous messages
+    self.context = zmq.Context()
+    self.server = self.context.socket(zmq.DEALER)  
+    # server = context.socket(zmq.ROUTER)  # if unique messaging is required between clients and  the App Manager
+    self.server.bind('tcp://*:9999')
 
-# def tprint(msg):
-#     """like print, but won't get newlines confused with multiple threads"""
-#     sys.stdout.write(msg + '\n')
-#     sys.stdout.flush()
+    # Simple PUB Socket for broadcasting messages to every clientApp {N} connected
+    self.server_pub_Socket = self.context.socket(zmq.PUB)
+    self.server_pub_Socket.bind('tcp://*:8888')
 
-# class ServerTask(threading.Thread):
-#     """ServerTask"""
-#     def __init__(self):
-#         threading.Thread.__init__ (self)
+  def run(self):
+    try:
+      # threads to start both receving and sending messages
+      # since we're using the command line for input from the user, using two threads
+      # to simulate this interaction between the AppManager and Client {N}. 
+      # A sending action is not prevented by a receiving action
+      t1 = threading.Thread(target=self.receiving_messages, args=())
+      t2 = threading.Thread(target=self.sending_messages, args=())
 
-#     def run(self):
-#         context = zmq.Context()
-#         frontend = context.socket(zmq.ROUTER)
-#         # frontend = context.socket(zmq.DEALER)
-#         frontend.bind('tcp://*:6670')
+      t1.daemon = True # die when the main thread dies
+      t2.daemon = True
+      t1.start()
+      t2.start()
 
-#         backend = context.socket(zmq.DEALER)
-#         backend.bind('inproc://BACKEND')
+      t1.join()
+      t2.join()
+      
+    except Exception as e:
+      print(e, " this is the error")
+      # Close the server socket
+      self.exit()
 
-#         workers = []
-#         for i in range(5):
-#             worker = ServerWorker(context)
-#             worker.start()
-#             workers.append(worker)
+  def exit(self):
+      """Close the socket"""
+      self.server.close()
+      self.server_pub_Socket.close()
+      self.context.term()
 
-#         zmq.proxy(frontend, backend)
+    # poll = zmq.Poller()
+    # poll.register(server, zmq.POLLIN)
+    # poll.register(server_pub_Socket, zmq.POLLIN)
 
-#         frontend.close()
-#         backend.close()
-#         context.term()
+  # sending message function
+  def sending_messages(self):
+    while True:
+      try:
+        message = input()
+        list_Msg = [bytes(message, 'utf-8')]
+        self.server_pub_Socket.send_multipart(list_Msg)
+        # server_pub_Socket.send_string(u'%s' % (message))
+        print("You: " + message)
 
-# class ServerWorker(threading.Thread):
-#     """ServerWorker"""
-#     def __init__(self, context):
-#         threading.Thread.__init__ (self)
-#         self.context = context
-
-#     def run(self):
-#         worker = self.context.socket(zmq.DEALER)
-#         worker.connect('inproc://BACKEND')
-#         tprint('Worker started')
-
-#         while True:
-#             # try:
-#             ident, msg  = worker.recv_multipart()
-#             time.sleep(1)
-#             print("Sending message...")
-#             self.receiveMessage(ident, msg)
-#             myreply = bytes("HEHEHEHE GOT YOU", 'utf-8')
-#             worker.send_multipart(myreply)
-#             worker.send(myreply)
-#                 # worker.send(b"HEHEHEEHEE GOT YOU :-)")
-#             # except KeyboardInterrupt:
-#             #     print("Quiting application...")
-#             # except Exception as e:
-#             #     print(e)
-#                 # worker.send_multipart(msg)
-#                 # worker.send(msg)
-
-
-#         worker.close()
-
-#     def receiveMessage(self, ident, msg):
-#         tprint('%s: %s' % (ident, msg))
-
-#     def sendMessage(self, identity, mess):
-#         while True:
-#             clientName = input("Client Name or ID to reply to: ")
-#             time.sleep(5)
+      except:
+        print("W: interrupt received, stopping...")
+        time.sleep(3)  # do some work...
+        break
+    self.context.term() # terminating here so the receiving connection also terminates 
+                        # thus causing the except block to get triggered for the receiving_messages function
 
 
-# def main():
-#     """main function"""
-#     server = ServerTask()
-#     server.start()
-#     server.join()
+  # receiving message function
+  def receiving_messages(self):
+    while True:
+      try:
+        client_Information = [msg.decode('utf-8') for msg in self.server.recv_multipart()]
+        client_ID           = client_Information[0]
+        client_Msg          = client_Information[1]
+        print(f"{client_ID}: {client_Msg}")
+      except Exception as e:
+        print("Terminating receiving connection...") # this gets triggered when the context is terminated
+        break
 
+if __name__ == '__main__':
+    #Create and start the AppManager
+    AppManager = AppManager()
+    AppManager.run()
 
-# if __name__ == "__main__":
-#     main()
-
-
-# ?????????????????????????????????????????????????????????????????????????????
-# ?????????????????????????????????????????????????????????????????????????????
-
-
-# Simple Router Socket
-context = zmq.Context()
-server = context.socket(zmq.DEALER)  
-# server = context.socket(zmq.ROUTER)  # Use this if you need to ID the clients to connecting to the App Manager
-server.bind('tcp://*:9999')
-
-# poller = zmq.Poller()
-# poller.register(server, zmq.POLLIN)
-
-
-server_pub_Socket = zmq.Context().socket(zmq.PUB)
-server_pub_Socket.bind('tcp://*:8888')
-
-
-def tprint(msg):
-    """like print, but won't get newlines confused with multiple threads"""
-    sys.stdout.write(msg + '\n')
-    sys.stdout.flush()
-
-
-# sending message function
-def sending_messages(server, server_pub_Socket):
-  while True:
-    message = input()
-    # server.send_string(u'%s' % (message))
-    # server.send_multipart([u'%s' % (message)])
-    list_Msg = [bytes(message, 'utf-8')]
-    server_pub_Socket.send_multipart(list_Msg)
-    # server_pub_Socket.send_string(u'%s' % (message))
-    print("You: " + message)
-
-# receiving message function
-def receiving_messages(server):
-  while True:
-    client_Information = [msg.decode('utf-8') for msg in server.recv_multipart()]
-    client_ID           = client_Information[0]
-    client_Msg          = client_Information[1]
-    print(f"{client_ID}: {client_Msg}")
-
-
-# threads to start both receving and sending functions
-threading.Thread(target=receiving_messages, args=(server, )).start()
-threading.Thread(target=sending_messages, args=(server, server_pub_Socket)).start()
-
-
-# TODO: Handle ctrl-C 
