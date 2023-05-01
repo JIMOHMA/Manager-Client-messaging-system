cc78c3ec942b016677f0b0181ad5d93805b13b46
Muyideen Jimoh <mail4ayodele@gmail.com>
2023-04-30 21:48:01 -0400

    Django Implementation Starter File

---
+++ Manager-Client-Django/websolution/websolution/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def manager(request):
+    context = {
+        userName:   "App 0 Manager", 
+        message:     "Welcome to my server" 
+    }
+    return render(request, manager.html, context=context)
+
+def client(request):
+    context = {
+        userName:   "client",
+        message:    "Requesting a file from the manager"
+    }
+    return render(request, client.html, context=context)
