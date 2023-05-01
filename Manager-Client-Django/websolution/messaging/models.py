59f4817dcb7c1269d1b00661e714915dfc2b2ce2
Muyideen Jimoh <mail4ayodele@gmail.com>
2023-05-01 12:29:00 -0400

    Implemented models for a given message

--- Manager-Client-Django/websolution/messaging/models.py
+++ Manager-Client-Django/websolution/messaging/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+  userName          = models.CharField(max_length=255)
+  message           = models.CharField(max_length=255)
+  messageTimeStamp  = models.TimeField(auto_now_add=False, blank=True)
